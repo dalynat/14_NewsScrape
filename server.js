a189a2446ffa70b23e8db8e5b75280262f99488c
Nathan Daly <dalynat@gmail.com>
2019-05-01 23:51:51 -0400

    fixed

---
+++ server.js
@@ -0,0 +1,25 @@
+// Require our dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
+var app = express();
+var routes = require("./routes");
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+// If deployed, use the deployed DB otherwise us local DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScrape";
+mongoose.connect(MONGODB_URI);
+
+// Listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
+});
